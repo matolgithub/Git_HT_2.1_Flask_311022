2e88f8b31e1c6540a724408b98f4b2113c40acc1
matolgithub <89175955650@mail.ru>
2022-10-31 22:01:36 +0300

    Commit: add requests.http, req..txt, server.py, views.py.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from flask import jsonify
 
+from views import AdView
+
 app = Flask("app")
 
 
@@ -10,6 +12,9 @@ def index_page():
     return render_template("index.html", title="Index_Title",
                            text="Main page of the website!", main_string="--Flask - it is very COOL!--")
 
+
+app.add_url_rule("/ads/<int:id_ad>/", view_func=AdView.as_view("ads_delete"), methods=["DELETE", "GET"])
+app.add_url_rule("/ads", view_func=AdView.as_view("ads_create"), methods=["POST"])
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
---
+++ server.py
@@ -0,0 +1,39 @@
+from typing import Union
+import pydantic
+from flask import Flask, jsonify
+
+app = Flask("app")
+
+
+class HTTPError(Exception):
+    def __init__(self, status_code: int, message: Union[str, list, dict]):
+        self.status_code = status_code
+        self.message = message
+
+
+@app.errorhandler(HTTPError)
+def handle_invalid_usage(error):
+    response = jsonify({'message': error.message})
+    response.status_code = error.status_code
+    return response
+
+
+class CreateAdModel(pydantic.BaseModel):
+    title: str
+    description: str
+    owner: str
+
+    @pydantic.validator("title")
+    def min_max_length(cls, value: str):
+        if len(value) > 31:
+            raise ValueError('Wrong title, must be less or equal than 30 characters!')
+        elif len(value) == "":
+            raise ValueError('Empty title!')
+        return value
+
+
+def validate(unvalidated_data: dict, validation_model):
+    try:
+        return validation_model(**unvalidated_data).dict()
+    except pydantic.ValidationError as er:
+        raise HTTPError(400, er.errors())
